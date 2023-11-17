9201a71e411b8f228f6d5fffb91cb839d05d2f29
Matias Damonte <mdamonte88@gmail.com>
2023-11-17 14:45:11 -0300

    Call to the APIs and show each ones of these values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,52 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import './styles/main.css'
 import styles from './styles/page.module.css'
 import StatusComponent from './components/StatusContainer'
+import StatusType from './types/status';
+import apiNames from './constants/apiConstants';
+import getStatues from './services/apiService'
 
 export default function Home() {
+  const [statuses, setStatuses] = useState<StatusType[]>([]);
+
+  const updateStatuses = async () => {
+    const statusPromises = apiNames.map(async (apiName) => {
+      return getStatues(apiName);
+    });
+  
+    try {
+      const results = await Promise.all(statusPromises);
+      
+      setStatuses(results);
+    } catch (error) {
+      console.error('Error fetching statuses:', error);
+    }
+  };
+
+  useEffect(() => {
+
+    updateStatuses();
+    const timeToUpdate = 10000;
+    const intervalId = setInterval(updateStatuses, timeToUpdate);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+
   return (
     <main className={styles.main}>
-      <StatusComponent
-          key='accounts'
-          apiName='accounts'
-          success={true}
-          message='Test success'
-          hostname='Test hostname'
-          time={0}
-      />
+      {statuses.map((status) => (
+        <StatusComponent
+          key={status.apiName}
+          apiName={status.apiName}
+          success={status.success}
+          message={status.message}
+          hostname={status.hostname}
+          time={status.time}
+        />
+      ))}
     </main>
   )
 }
