d27c427aece9e109b26e45fac0ec2876388611fa
Matias Damonte <mdamonte88@gmail.com>
2023-11-17 14:30:07 -0300

    Create the StatusContainer component

---
+++ src/app/components/StatusContainer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import StatusType from '../types/status';
+import styles from '../styles/statusContainer.module.css'; // Import the CSS module
+
+const StatusComponent: React.FC<StatusType> = ({ apiName, success, message, hostname, time }) => {
+  const apiNameUpper = apiName.toUpperCase();
+  const [title, description] = message.split(':');
+
+  return (
+    <div className={styles['status-container']}>
+      <div>
+        <h2>{apiNameUpper}</h2>
+        {success && (
+          <>
+            <p className={styles.success}>{title}</p>
+            <p className='small'>{hostname}</p>
+            <p className='small'>{time}</p>
+          </>
+        )}
+
+        {!success && (
+          <>
+            <p className="error">Error</p>
+            <p className='large red'>OUTAGE</p>
+            <p className='small red'>{title}</p>
+            <p className='small red'>{description}</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+  
+  export default StatusComponent;
