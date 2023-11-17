bfeb53fa704ec63c874161805b03aa8906c10817
Matias Damonte <mdamonte88@gmail.com>
2023-11-17 16:22:05 -0300

    Create a Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="App-header">
+        <span> Status Dashboard </span>
+    </header>
+  );
+};
+
+export default Header;
